2de464c876a96b73e071918aa6597735059cbe4d
Kimbugwe Simon Peter <kimbsimon2@gmail.com>
2019-05-11 17:03:18 +0300

    ch(models): add sql queries
    - add sql file to add user, movies and reservations
    - add method to execute the sql queries

--- src/models/index.js
+++ src/models/index.js
@@ -1,6 +1,24 @@
 const pool = require("../../config/default")
 let connection = pool("default")
+var batch = require("batch")
+var fs = require("fs");
 
-connection.query("CREATE TABLE if not exists movies (id SERIAL PRIMARY KEY,title text,year text)")
+async function processSQLFile(fileName) {
 
+    // Extract SQL queries from files. Assumes no ';' in the fileNames
+    var queries = fs.readFileSync(fileName).toString()
+        .replace(/(\r\n|\n|\r)/gm, " ") // remove newlines
+        .replace(/\s+/g, " ") // excess white space
+        .split(";") // split into all statements
+        .map(Function.prototype.call, String.prototype.trim)
+        .filter(function (el) { return el.length != 0 }); // remove any empty ones
+    queries.forEach((query) => {
+        connection.query(query)
+            .then(
+                console.log("success")
+            );
+    })
+}
+
+processSQLFile("src/models/database.sql")
 module.exports = connection
